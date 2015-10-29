475a71ea7bd885a7e6adf96934071db987d62b86
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2015-10-29 21:22:40 +0200

    cleaned up

--- base.c
+++ base.c
@@ -22,25 +22,33 @@ SIGNAL(TIM0_COMPA_vect)
 	PORTA=(r>cnt)|((g>cnt)<<1)|((b>cnt)<<2)|((w>cnt)<<3);
 }
 
-void rgb(uint32_t color)
+void rgb(uint32_t color, uint8_t bright)
 {
+	static float rr=0,gg=0,bb=0;
 	while(color>3*RESOLUTION)
 		color-=3*RESOLUTION;
 	if(color<=RESOLUTION)
 	{
-		r=RESOLUTION-color;
-		g=color;
+		rr=RESOLUTION-color;
+		gg=color;
 	}
 	else if(color>RESOLUTION && color<=2*RESOLUTION)
 	{
-		g=2*RESOLUTION-color;
-		b=color-RESOLUTION;
+		gg=2*RESOLUTION-color;
+		bb=color-RESOLUTION;
 	}
 	else
 	{
-		b=3*RESOLUTION-color;
-		r=color-2*RESOLUTION;
+		bb=3*RESOLUTION-color;
+		rr=color-2*RESOLUTION;
 	}
+	rr=rr*bright/RESOLUTION;
+	gg=gg*bright/RESOLUTION;
+	bb=bb*bright/RESOLUTION;
+
+	r=(uint8_t) rr;
+	g=(uint8_t) gg;
+	b=(uint8_t) bb;
 }
 
 int main(void)
@@ -48,8 +56,6 @@ int main(void)
 	TCCR0B = (1<<CS00);	//no prescale
 	TIMSK0 = (1<<OCIE0A);	//interrupt on compare match
 	OCR0A = 0xf;	//interrupt every 0xf cycles
-	//TCCR0 |= (1<<CS00);	//timer/counter0 with no prescale
-	//TIMSK |= (1<<TOIE0);	//enable interrupt for timer/counter0
 	sei();	//enable interrupts
 	
 	DDRA = 0xf;	//first 4 pins out
@@ -59,16 +65,9 @@ int main(void)
 	pwm=0;
 	srand(1);
 	uint32_t goal = 0, current=0;
-	uint32_t i;
 	while(1)
-	{	
-		/*for(i = 0 ; i<3*RESOLUTION; i++)
-		{
-			rgb(i);
-			_delay_ms(5);
-		}*/
-		_delay_ms(5);
-		rgb(current);
+	{
+		rgb(current,RESOLUTION);
 		if(current<goal)
 			current++;
 		else if(current>goal)
